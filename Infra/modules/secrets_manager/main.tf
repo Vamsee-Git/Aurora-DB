4638fe02d1289da15b64ec3b7fb5d7da310b2cf6
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-04-20 18:46:24 +0530

    Update main.tf

--- Infra/modules/secrets_manager/main.tf
+++ Infra/modules/secrets_manager/main.tf
@@ -1,9 +1,12 @@
 resource "aws_secretsmanager_secret" "db_secret" {
-  name         = var.secret_name
-  secret_string = var.secret_string
+  name = var.secret_name
 }
 
 resource "aws_secretsmanager_secret_version" "db_secret_version" {
   secret_id     = aws_secretsmanager_secret.db_secret.id
   secret_string = var.secret_string
+}
+
+output "db_secret_id" {
+  value = aws_secretsmanager_secret.db_secret.id
 }
