3dfe66a3e4aff33ef17e6c54eceee2ab8f694c8d
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-04-20 17:59:47 +0530

    Create main.tf

---
+++ Infra/modules/aurora_db/main.tf
@@ -0,0 +1,15 @@
+resource "aws_rds_cluster" "aurora" {
+  cluster_identifier      = var.db_cluster_identifier
+  engine                  = "aurora-mysql"
+  master_username         = var.db_username
+  master_password         = var.db_password
+  backup_retention_period = 5
+  preferred_backup_window = "07:00-09:00"
+}
+
+resource "aws_rds_cluster_instance" "aurora_instances" {
+  count              = 2
+  identifier         = "${var.db_cluster_identifier}-instance-${count.index}"
+  cluster_identifier = aws_rds_cluster.aurora.id
+  instance_class     = var.db_instance_class
+}
